9c979516faaf5c1d0ffcd452d9f4e8530cde866a
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-07 16:25:55 -0300

    fix: Force DATABASE_URL in production environment config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.database_configuration = { "production" => { "url" => ENV["DATABASE_URL"] } }
+
