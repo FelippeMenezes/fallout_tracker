3cc1613242b108657d32d543f4ff0cb4a198f63a
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-07 13:32:32 -0300

    All basic project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
+# app/controllers/application_controller.rb
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # ADICIONE ESTE MÉTODO AQUI
+  protected
+
+  def after_sign_in_path_for(resource)
+    # Esta linha diz ao Devise para onde ir após o login.
+    # Neste caso, para a sua lista de itens (items_path).
+    items_path
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_07_161434) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_07_161912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "rarity"
+    t.boolean "owned"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
