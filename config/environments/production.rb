8b30ff58cd67eabb4f9be1003260bc040376635d
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-07 16:31:37 -0300

    fix: Move database config inside Rails.application.configure block

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.database_configuration = { "production" => { "url" => ENV["DATABASE_URL"] } } if ENV["DATABASE_URL"]
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
