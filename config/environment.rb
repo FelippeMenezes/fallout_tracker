7dff7527bb4ee151f4b08743add4e7c11864b083
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-07 16:45:07 -0300

    Final attempt to fix production database connection

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.database_configuration = { "production" => { "url" => ENV["DATABASE_URL"] } }
-
