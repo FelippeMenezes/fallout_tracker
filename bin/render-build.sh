2fb2165451368b28604371f864f7165036be457f
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-07 21:14:19 -0300

    Configura projeto para deploy no Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,18 @@
 # exit on error
 set -o errexit
 
+echo "Iniciando o build..."
+
+# Instala as dependências do Ruby
 bundle install
+
+# Precompila os assets (CSS, JavaScript)
 bundle exec rails assets:precompile
+
+# Limpa assets antigos para economizar espaço
 bundle exec rails assets:clean
+
+# Roda as migrações do banco de dados
 bundle exec rails db:migrate
+
+echo "Build finalizado."
